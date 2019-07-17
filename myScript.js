6bcbf6fedb5957c47cdb53ce9e03a661e3a4656d
Charlay0012 <csullivan6178@schk12.us>
2019-07-17 10:35:55 -0500

    First commit

---
+++ myScript.js
@@ -0,0 +1,33 @@
+function showTime(){
+  document.getElementById('demo').innerHTML = Date()
+}
+
+
+// alert("Hello World")
+
+// function timesThree(num) {
+//     return num * 3;
+// };
+// function timesFive(num) {return num * 5;};
+//
+//
+// if (5==5) {
+//   alert("Yay!");
+// };
+
+// let text = ""
+//
+// for (var i = 0; i < 5; i++) {
+//     text += "Num: " + i;
+// }
+// let num = 0
+// for (var i = 0; i < 5; i++){
+//   num += i
+// }
+
+// var number = -25
+//
+// var age = 16;
+// age = age + 1;
+//
+// console.log("Hello World")
